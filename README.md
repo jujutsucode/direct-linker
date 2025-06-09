7616243d47b1b6bcde4180be1fd6306c6039e447
Kosyachnik <atikeone9@gmail.com>
2025-06-09 23:50:35 +0300

    Init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# 🤖 Direct Linker 
+## Overview
+Direct Linker is a FastApi Server for upload files and get direct links to download.
+
+## Project Structure
+- `app.py`: Server on FastApi.
+- `templates/index.html`: View.
+- `uploads/`: Uploaded files folder.
+
+## Requirements
+- `Python 3.6+`: based?
+- `FastApi`: FastAPI is a modern, fast (high-performance), web framework
+- `uvicorn`: An ASGI web server, for Python
+- `Jinja2`: is a fast, expressive, extensible templating engine
+
+## How to Usage ❓
+1. Execute `python app.py` in project directory
+2. Goto `http://127.0.0.1:5000` in your browser
+3. Upload any file and get direct link to download
+4. You are the best
---
+++ app.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import os
+import uuid
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="File not attached")
+    
+    file_id = str(uuid.uuid4())
+    file_extension = os.path.splitext(file.filename)[1]
+    filename = f"{file_id}{file_extension}"
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+    
+    download_url = f"/download/{filename}"
+    return {"url": download_url}
+
+@app.get("/download/{filename}")
+async def return_file(filename: str):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return RedirectResponse(url=f"/uploads/{filename}")
+
+app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name=UPLOAD_DIR)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
