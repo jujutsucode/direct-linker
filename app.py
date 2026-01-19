217aaee907149bcfab3ea115cd1dfb24e8827468
dwix <138183266+jujutsucode@users.noreply.github.com>
2026-01-19 08:07:27 +0700

    Add Response import from FastAPI

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Request
+from starlette.types import Scope
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
